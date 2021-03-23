a9419cff5eb27bc11306ef94588629a9989a7315
siwon405 <on.siwon@gmail.com>
2021-03-23 22:41:32 +0900

    1'st 완성

--- app/src/main/java/com/on/myapplication/Tab2Fragment.java
+++ app/src/main/java/com/on/myapplication/Tab2Fragment.java
@@ -4,30 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
-    private Button btnTEST;
+    private ListView mListView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment2_layout,container,false);
-        btnTEST = view.findViewById(R.id.btnTEST);
+        mListView = (ListView) view.findViewById(R.id.listView);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "new 토스트 출시", Toast.LENGTH_SHORT).show();
+        ArrayList<Card> list = new ArrayList<>();
 
-            }
-        });
+        list.add(new Card("drawable://" + R.drawable.a, "a 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.b, "b 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.c, "c 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.d, "d 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.e, "e 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.f, "f 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.g, "g 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.h, "h 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.i, "i 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.j, "j 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.k, "k 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.l, "l 이미지 입니다 "));
+        list.add(new Card("drawable://" + R.drawable.m, "m 이미지 입니다 "));
+
+        CustomListAdapter adapter = new CustomListAdapter(getActivity(), R.layout.card_layout_main, list);
+        mListView.setAdapter(adapter);
+
+
         return view;
     }
 }
